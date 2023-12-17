f7f78065896bff1e2e2fd3e622426366a1c32fe5
Zhixiang <2616718f@student.gla.ac.uk>
2023-12-17 12:25:07 +0000

    Feature: Claim bread and rob bread

---
+++ group_reply.py
@@ -0,0 +1,123 @@
+import asyncio
+import os
+import json
+from datetime import datetime
+import random
+
+import botpy
+from botpy import logging
+from botpy.ext.cog_yaml import read
+from botpy.message import GroupMessage, Message
+
+test_config = read(os.path.join(os.path.dirname(__file__), "config.yaml"))
+bread = os.path.join(os.path.dirname(__file__), "bread.json")
+
+_log = logging.get_logger()
+
+
+# {"userid": {"number": 1, "last_claim": 1, "id": "id", "last_rob" = 0}}
+
+class MyClient(botpy.Client):
+    with open(bread, 'a+') as file:
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError:
+            data = {}
+
+    async def on_ready(self):
+        _log.info(f"robot 「{self.robot.name}」 on_ready!")
+
+    async def on_group_at_message_create(self, message: GroupMessage):
+        now = datetime.now()
+        user = message.author.member_openid
+        content = message.content.strip().split()
+        if len(content) == 0:
+            return
+        if content[0] == "买面包":
+            if not (user in self.data):
+                if content[0] == "买面包" and isinstance(content[1], str):
+                    if not any(user_data.get('id') == content[1] for user_data in self.data.values()):
+                            self.data[user] = {"number": 0, 'last_claim': 0, "id": content[1]}
+                    else:
+                        messageResult = await message._api.post_group_message(
+                            group_openid=message.group_openid,
+                            msg_type=0,
+                            msg_id=message.id,
+                            content=f"id已被占用"
+                        )
+                else:
+                    messageResult = await message._api.post_group_message(
+                        group_openid=message.group_openid,
+                        msg_type=0,
+                        msg_id=message.id,
+                        content=f"格式错误：买面包 <id> (id 不能有空格)"
+                    )
+            else:
+                messageResult = await message._api.post_group_message(
+                    group_openid=message.group_openid,
+                    msg_type=0,
+                    msg_id=message.id,
+                    content=f"\n第一次买面包需要附上昵称: 买面包 <id> (id 不能有空格)"
+                )
+            if user in self.data:
+                if (now.weekday() == 5 or now.weekday() == 6) and (
+                        (now.timestamp() - int(self.data[user]["last_time"])) > 36000):
+                    number_obtained = random.randint(1, 3)
+                    self.data[user]["number"] += number_obtained
+                    self.data[user]["last_time"] = now.timestamp()
+                    with open(bread, 'a+') as file:
+                        json.dump(self.data, file)
+                        file.flush()
+
+                    messageResult = await message._api.post_group_message(
+                        group_openid=message.group_openid,
+                        msg_type=0,
+                        msg_id=message.id,
+                        content=f"\n买了{number_obtained}个面包\n当前拥有{self.data[user]['number']}个面包"
+                    )
+                else:
+                    messageResult = await message._api.post_group_message(
+                        group_openid=message.group_openid,
+                        msg_type=0,
+                        msg_id=message.id,
+                        content=f"现在还不能买面包哦~"
+                    )
+        elif content[0] == "抢面包":
+            if len(content) >= 2:
+                if any(user_data.get('id') == content[1] for user_data in self.data.values()):
+                    robbed = random.randint(1, 3)
+                    for user_data in self.data.values():
+                        if user_data.get('id') == content[1]:
+                            if user_data['number'] - robbed > 0:
+                                user_data['number'] -= robbed
+                                self.data[user]['number'] += robbed
+                                with open(bread, 'a+') as file:
+                                    json.dump(self.data, file)
+                                    file.flush()
+
+                                messageResult = await message._api.post_group_message(
+                                    group_openid=message.group_openid,
+                                    msg_type=0,
+                                    msg_id=message.id,
+                                    content=f"\n抢了{robbed}个面包\n当前拥有{self.data[user]['number']}个面包\n{content[1]}还剩{user_data['number']}个面包"
+                                )
+                            else:
+                                messageResult = await message._api.post_group_message(
+                                    group_openid=message.group_openid,
+                                    msg_type=0,
+                                    msg_id=message.id,
+                                    content=f"\n{content[1]}太穷了，抢抢别人吧qaq"
+                                )
+
+            # _log.info(messageResult)
+            # print(f"author:{message.author} \n content:{message.content} \n id:{message.id} "
+            #       f"\n group_openid:{message.group_openid} \nevent_id:{message.event_id}"
+            #       f"attachment: {message.attachments}\n mentions:{message.mentions}"
+            #       f"\n reference: {message.message_reference} \n seq: {message.msg_seq}")
+            # print(messageResult)
+
+
+if __name__ == "__main__":
+    intents = botpy.Intents(public_messages=True)
+    client = MyClient(intents=intents)
+    client.run(appid=test_config["appid"], secret=test_config["secret"])
